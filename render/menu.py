9526e1c140cb92f22edbbbada60281311dce85f8
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2026-04-12 16:22:52 -0600

    feat: rotating tank

--- render/menu.py
+++ render/menu.py
@@ -2,62 +2,67 @@ import pygame as pg
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 
-from render.models import draw_tank_2d
+from render.models import draw_tank_rotating
 from render.utils import begin_draw_2d, end_draw_2d
+
 
 def display_menu(display_w, display_h):
     menu_open = True
-    
+
     while menu_open:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
-        
+
         begin_draw_2d(display_w, display_h)
 
         glColor3f(0.0, 1.0, 0.0)
-        
+
         # Title
         glRasterPos2f(10, 25)
         for char in "Tio's Battlezone":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
-        
+
         # Tank Drawing
-        draw_tank_2d(display_w, display_h)
-        
+        draw_tank_rotating(display_w, display_h)
+
         # Control Instructions
         glRasterPos2f(10, 50)
-        for char in 'Controls: W/S to Move, A/D to Rotate, SPACE to Shoot':
+        for char in "Controls: W/S to Move, A/D to Rotate, SPACE to Shoot":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(char))
         glRasterPos2f(10, 70)
-        for char in 'Shoot tanks to score points and advance levels. Avoid taking damage to survive!':
+        for (
+            char
+        ) in "Shoot tanks to score points and advance levels. Avoid taking damage to survive!":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(char))
-        
+
         # Game options
         glRasterPos2f(10, 150)
-        for char in '1. Start Game (Easy)':
+        for char in "1. Start Game (Easy)":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         glRasterPos2f(10, 175)
-        for char in '2. Start Game (Normal)':
+        for char in "2. Start Game (Normal)":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         glRasterPos2f(10, 200)
-        for char in '3. Start Game (Hard)':
+        for char in "3. Start Game (Hard)":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         glRasterPos2f(10, 225)
-        for char in 'Press 1, 2, or 3 to Start':
+        for char in "Press 1, 2, or 3 to Start":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         glRasterPos2f(10, 275)
-        for char in 'Press ESC to Quit':
+        for char in "Press ESC to Quit":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
-            
+
         # Copyright
         glRasterPos2f(10, display_h - 80)
-        for char in 'This is a 3D graphics project inspired by the original Battlezone game.':
+        for (
+            char
+        ) in "This is a 3D graphics project inspired by the original Battlezone game.":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(char))
         glRasterPos2f(10, display_h - 60)
-        for char in 'Original game copyright © 1983 Atari. All rights reserved.':
+        for char in "Original game copyright © 1983 Atari. All rights reserved.":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(char))
         glRasterPos2f(10, display_h - 40)
-        for char in 'Code and New Assets © 2024. All rights reserved.':
+        for char in "Code and New Assets © 2024. All rights reserved.":
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(char))
 
         end_draw_2d()
@@ -73,4 +78,3 @@ def display_menu(display_w, display_h):
                 if event.key == pg.K_2:
                     pg.quit()
                     exit()
-    
--- render/models.py
+++ render/models.py
@@ -3,6 +3,8 @@ from render.utils import load_obj
 
 tank_vertices, _, tank_lines = load_obj("models/tank_wireframe.obj")
 
+tank_rotation = 0.0
+
 def draw_tank_3d(enemy_tank):
 
     glPushMatrix()
@@ -26,7 +28,74 @@ def draw_tank_3d(enemy_tank):
 
     glPopMatrix()
 
+
 def draw_tank_2d(display_w, display_h):
+
+    glPushMatrix()
+
+    glTranslatef(display_w // 2, display_h // 2, 0)
+    glRotatef(180, 0, 1, 0)
+    glScalef(100, -100, 1)
+    glColor3f(0.0, 1.0, 0.0)
+    glLineWidth(2.0)
+
+    glBegin(GL_LINES)
+
+    for line in tank_lines:
+        for i in range(len(line) - 1):
+            v1 = tank_vertices[line[i]]
+            v2 = tank_vertices[line[i + 1]]
+
+            glVertex2f(v1[0], v1[1])
+            glVertex2f(v2[0], v2[1])
+
+    glEnd()
+
+    glPopMatrix()
+
+
+def draw_tank_flattened_3d(display_w, display_h):
+    glPushMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+
+    glOrtho(
+        -display_w // 2, display_w // 2, display_h // 2, -display_h // 2, -1000, 1000
+    )
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glTranslatef(0, 0, 0)
+    glRotatef(30, 1, 0, 0)  # tilt towards screen
+    glRotatef(200, 0, 1, 0)  # horizontal turn
+    glScalef(100, -100, 100)  # scale all 3 axes
+
+    glColor3f(0.0, 1.0, 0.0)
+    glLineWidth(2.0)
+
+    glBegin(GL_LINES)
+
+    for line in tank_lines:
+        for i in range(len(line) - 1):
+            v1 = tank_vertices[line[i]]
+            v2 = tank_vertices[line[i + 1]]
+
+            glVertex3f(v1[0], v1[1], v1[2])
+            glVertex3f(v2[0], v2[1], v2[2])
+
+    glEnd()
+
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+    glPopMatrix()
+
+def draw_tank_rotating(display_w, display_h):
+    global tank_rotation
+    tank_rotation += 0.5
+
     glPushMatrix()
     glMatrixMode(GL_PROJECTION)
     glPushMatrix()
@@ -38,23 +107,20 @@ def draw_tank_2d(display_w, display_h):
     glLoadIdentity()
 
     glTranslatef(0, 0, 0)
-    glRotatef(30, 1, 0, 0)   # tilt towards screen
-    glRotatef(200, 0, 1, 0)  # horizontal turn
-    glScalef(100, -100, 100) # scale all 3 axes
+    glRotatef(30, 1, 0, 0)            # tilt towards screen (fixed)
+    glRotatef(tank_rotation, 0, 1, 0) # horizontal spin (animated)
+    glScalef(100, -100, 100)
 
     glColor3f(0.0, 1.0, 0.0)
     glLineWidth(2.0)
 
     glBegin(GL_LINES)
-    
     for line in tank_lines:
         for i in range(len(line) - 1):
             v1 = tank_vertices[line[i]]
             v2 = tank_vertices[line[i + 1]]
-            
-            glVertex3f(v1[0], v1[1], v1[2])  
+            glVertex3f(v1[0], v1[1], v1[2])
             glVertex3f(v2[0], v2[1], v2[2])
-            
     glEnd()
 
     glMatrixMode(GL_PROJECTION)
