ce852152599825f8fbddb8e4c944a965b45fdfe7
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2026-04-12 18:24:12 -0600

    refactor: change hud look

--- render/hud.py
+++ render/hud.py
@@ -8,35 +8,41 @@ def draw_hud(game, dw, dh):
     init_enemies = game.world.init_enemy_count
     
     begin_draw_2d(dw, dh)
-    draw_level(game.world.level)
+    draw_level(game.world.level, dw)
+    draw_difficulty(game.world.difficulty, dw)
     draw_score(game.score)
     draw_health(game.health)
     draw_enemy_count(curr_enemies, init_enemies)
     draw_minimap()
     end_draw_2d()
-    
-    
-def draw_level(level):
-    glColor3f(0.0, 1.0, 0.0)
-    glRasterPos2f(10, 25)
+        
+def draw_difficulty(difficulty, dw): 
+    glColor3f(0.5, 0.7, 1.0)
+    glRasterPos2f(dw-150, 25)
+    for char in f'Difficulty: {difficulty.capitalize()}':
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
+        
+def draw_level(level, dw):
+    glColor3f(1.0, 1.0, 1.0)
+    glRasterPos2f(dw-150, 50)
     for char in f'Level: {level}':
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         
 def draw_score(score):
-    glColor3f(1.0, 1.0, 1.0)
-    glRasterPos2f(10, 50)
+    glColor3f(0.0, 1.0, 0.0)
+    glRasterPos2f(10, 25)
     for char in f'Score: {score}':
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         
 def draw_health(health):
     glColor3f(1.0, 0.0, 0.0)
-    glRasterPos2f(10, 75)
+    glRasterPos2f(10, 50)
     for char in f'Health: {health}':
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         
 def draw_enemy_count(curr, init):
     glColor3f(1.0, 1.0, 0.0)
-    glRasterPos2f(10, 100)
+    glRasterPos2f(10, 75)
     for char in f'Enemies: {curr}/{init}':
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(char))
         
